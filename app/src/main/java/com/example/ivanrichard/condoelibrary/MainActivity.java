46d0b278f071a084465c1ebb95c492164b25ba2d
ugolole <ugololeivan@yahoo.com>
2018-06-16 01:08:13 -0400

    Android UI design for a login screen

---
+++ app/src/main/java/com/example/ivanrichard/condoelibrary/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ivanrichard.condoelibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //the method is the first to be execute when the application runs
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
