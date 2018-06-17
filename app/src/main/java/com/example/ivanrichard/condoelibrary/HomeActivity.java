390ad29d9cc467dc658cf7dc3661c4ca6253570e
ugolole <ugololeivan@yahoo.com>
2018-06-17 13:40:43 -0400

    adding animation

--- app/src/main/java/com/example/ivanrichard/condoelibrary/HomeActivity.java
+++ app/src/main/java/com/example/ivanrichard/condoelibrary/HomeActivity.java
@@ -2,12 +2,28 @@ package com.example.ivanrichard.condoelibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
+//This will contain the code that allows users to access the applications
+//services.
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //Create and use a button to navigate to the next page
+        Button sign_up = (Button)findViewById(R.id.sign_up);
+
+        //use the button to open a new page
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this,SignUp.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ivanrichard/condoelibrary/MainActivity.java
+++ app/src/main/java/com/example/ivanrichard/condoelibrary/MainActivity.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 2000;
+    private static int SPLASH_TIME_OUT = 1000;
 
     //the method is the first to be execute when the application runs
     @Override
@@ -14,6 +17,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Accessing the image on the main page
+        ImageView image = (ImageView)findViewById(R.id.animation_image);
+
+        //create animation object and specify the animation directory
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.rotate);
+
+        //start the animation by specifying the object that needs animation start
+        image.startAnimation(animation);
+
+        //creating a delay before the next screen is open.
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
